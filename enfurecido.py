6a9c6c27e8d86cf5ee9f065d72bb45543bf90cda
HenriqueTardivo <henrique.tardivo@gmail.com>
2020-05-21 05:01:49 -0300

    adiciona o encode utf8

--- enfurecido.py
+++ enfurecido.py
@@ -1,20 +1,21 @@
 import tweepy
 import time
 
-auth = tweepy.OAuthHandler('Dxw05fIkKdSRhLD1aiPTMbAt7',
-                           'NpA6jhO7zvSkxcq1kHXlj5Yj67BsO7X3m1QpSTsJXFS2QitoJL')
-auth.set_access_token('1247064631484571651-GtsKB6FNYesVuB1C90DQ0ar6TswuLJ',
-                      'xAFB40Mj2fpWuwikgPDsnDc1iuMj8EgS3XmjphxTwZrh0')
+auth = tweepy.OAuthHandler('Ef3mbrQa2j83BFexT4xR1D5F5',
+                           'rpjHvCtG4w9CFUmsaJOCOoGw4gqX4e7m504XhrfOJLldUvIegJ')
+auth.set_access_token('1247064631484571651-ZkVHbqrC5JcBjnKXJcRArJkGRGPGwm',
+                      'L0H3bW0RBjIBrMVwa4yHHRZEg0lvQfUWxwgwqCWezD2Ah')
 
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 search = 'amarelo'
-numero = 10000000
+numero = 1000
 
 for tweet in tweepy.Cursor(api.search, search).items(numero):
     try:
-        if((tweet.text)=='amarelo'):
+        print(tweet.text.encode("utf-8"))
+        if((tweet.text.encode("utf-8"))=='amarelo'):
             print("nome do usuario: @" + tweet.user.screen_name)
             api.update_status("@" + tweet.user.screen_name + " concordo", in_reply_to_status_id=tweet.id)
             print("tuite enviado corretamente")
